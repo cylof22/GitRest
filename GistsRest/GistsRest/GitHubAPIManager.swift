3f2cefe64284e479401b7b2b671f645684f24a43
cylof22 <cylof22@gmail.com>
2017-03-12 20:08:54 +0800

    Add GitHub Manager class

---
+++ GistsRest/GistsRest/GitHubAPIManager.swift
@@ -0,0 +1,16 @@
+//
+//  GitHubAPIManager.swift
+//  GistsRest
+//
+//  Created by 曹元乐 on 2017/3/12.
+//  Copyright © 2017年 曹元乐. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class GitHubAPIManager
+{
+    static let sharedInstance = GitHubAPIManager();
+}
